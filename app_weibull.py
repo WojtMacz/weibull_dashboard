e585fa8baba1b6f1b258069bf08ba87833d1494b
WojtMacz <71345215+WojtMacz@users.noreply.github.com>
2023-08-08 15:28:43 +0200

    Update app_weibull.py

--- app_weibull.py
+++ app_weibull.py
@@ -48,7 +48,73 @@ with st.sidebar:
     st.markdown(''' --- ''')
 
 if opcje == 'Wprowadzenie do analizy Weibulla':
-    st.write('Tu będą informacje na temat analizy Weibulla')
+    st.markdown('### Wprowadzenie do analizy niezawodności')
+    st.write("""
+             #### Rozkład Weibulla\n
+             Podczas analizy niezawodności, zajmujemy się 2 parametrową funkcją gęstości prawdopodobieństwa weibulla zdefiniowaną jako:
+             """)
+    st.latex(r'''
+             F(x) = \beta \eta(x \eta)^{\beta - 1} e^{-\frac{x}{\eta^\beta}}
+             ''')
+    st.markdown("""
+             gdzie:
+             - β lub *beta* reprezentuje parametr kształtu
+             - η lub *eta* reprezentuje parametr skali
+             - x reprezentuje wartość, przy której funkcja ma zostać obliczona\n
+             Gdyby wykreślić powyższą funkcję F(x) z danym  β i  η otrzymalibyśmy funkcję gęstości prawdopodobieństwa, powszechnie skracaną do PDF.\n
+             Z samego PDF można wyprowadzić funkcję rozkładu skumulowanego (tzw. CDF i funkcje hazardu), wraz z funkcją przeżycia, która jest bardzo przydatna w inżynierii niezawodności.\n
+             Parametr **kształtu**, β określa ogólny kształt rozkładu. Istnieją trzy podstawowe obszary, w których β może się znajdować:\n
+             - β<1.0 Wskazuje na "okres wieku dziecięcego" lub malejące awarie wraz z upływem czasu. Jest to rozkład, który można zaobserwować, gdy występuje zjawisko takie jak utwardzanie kleju. W miarę utwardzania kleju produkt ulega mniejszej liczbie awarii.
+             - β=1.0 Oznacza "losowe" lub "stałe" awarie. Ten rodzaj rozkładu jest najczęściej stosowany do niektórych kategorii komponentów elektronicznych, takich jak półprzewodniki.
+             - β>1.0 Wskazuje na styl rozkładu zużycia. Rozkład ten jest powszechnie obserwowany w przypadku elementów takich jak łożyska, których awaryjność wzrasta wraz ze wzrostem zużycia.
+             """)
+    st.image('beta-effects-on-pdf.png')
+    st.markdown(""" 
+                Możliwe jest, aby produkt wykazywał wszystkie te trzy cechy w odniesieniu do różnych komponentów. Wyobraźmy sobie samochód, który ma materiały klejące, elektronikę i łożyska, z których każdy ma swój własny rozkład awarii. 
+                Dzięki dokładnej analizie trybów awarii możliwe jest określenie rozkładów awarii poszczególnych komponentów, co pozwala projektantowi na potencjalną identyfikację i rozwiązanie najbardziej problematycznych elementów projektu w pierwszej kolejności.
+                """)
+    st.image('beta-effects-on-fr.png')
+    st.markdown(""" 
+                To właśnie połączenie tych trzech potencjalnie różnych charakterystyk β daje początek tradycyjnej krzywej wannowej powszechnie obserwowanej w analizie niezawodności.
+                Przyglądając się bliżej wykresowi wskaźników awaryjności, można zauważyć, że produkt składający się z trzech różnych komponentów o charakterystyce βmoże zacząć tworzyć ogólną krzywą wannową złożonych wskaźników awaryjności
+                """)
+    st.image('bathtub-components.png')
+    st.markdown(""" 
+                Parametr **skali**, η określa skalę rozkładu. 
+                Parametr ten jest również znany jako charakterystyczna żywotność produktu i odpowiada cyklom, w których 63% produktu uległo awarii
+                """)
+    st.image('eta-effects-on-pdf.png')
+    st.markdown(""" 
+                Należy zauważyć, że zmiany współczynnika skali zachowują kształt, ale skutecznie zmieniają długość, na którą rozkład jest "rozciągnięty". 
+                Zmiana wysokości wynika z faktu, że obszar pod krzywą musi być zawsze równy 1,0
+                """)
+    st.markdown(""" 
+                #### Określenie β i η\n
+                Pierwszym zadaniem inżyniera niezawodności jest określenie odpowiednich wartości dla β i η. 
+                Dwie najczęstsze opcje to:
+                - wykreślenie punktów na papierze Weibulla, aproksymacja linii, wyznaczenie nachylenia i czasu życia charakterystyki
+                - oszacowanie maksymalnego prawdopodobieństwa (MLE)\n
+                Ogólnie rzecz biorąc, jeśli dostępna jest tylko niewielka liczba punktów danych, przybliżenie za pomocą papieru Weibulla lub jego odpowiednika da dobre wyniki. Jeśli dostępna jest stosunkowo duża liczba danych, preferowane są metody MLE. Obie metody generalnie dają akceptowalne wyniki, zakładając, że rozkład Weibulla odpowiednio opisuje proces.
+                Obecnie aplikacja ta wykorzystuje techniki regresji liniowej (LR) i estymacji największej wiarygodności (MLE) do szacowania β i η. 
+                Ogólnie rzecz biorąc, MLE jest preferowana dla większych zestawów danych, podczas gdy LR jest preferowana dla mniejszych zestawów danych (mniej niż 15). 
+                Badanie dopasowania jest nadal niezbędne w obu metodach, aby określić, czy model rzeczywiście odpowiednio pasuje do danych.\n
+                #### Poziomy zaufania\n
+                Teraz, gdy masz już pewne β i η co to oznacza? Jaką masz pewność, że β i η są zgodne z obliczeniami? 
+                W końcu prawie na pewno istnieje pewien poziom zmienności danych. Wprowadzamy limity ufności. 
+                Krótko mówiąc, większość limitów ufności dla danych statystycznych zakłada rozkład normalny po prawej lub lewej stronie krzywej. 
+                Tak więc, na przykład, jeśli patrzysz na wykres prawdopodobieństwa, zakładasz, że większość punktów będzie blisko linii, podczas gdy mniejsza liczba punktów będzie dalej od linii. 
+                Odległość od linii będzie znajdować się w rozkładzie normalnym rozciągniętym wzdłuż linii.
+
+                """)
+    st.image('weibull-fit-10pt.png')
+    st.markdown(""" 
+                Aplikacja ta integruje limity ufności przy obliczaniu β i η w taki sposób, że granice ufności określają zakres pewności dla β i η niezależnie. 
+                Na przykład, jeśli granica ufności wynosi 0,95, to nasze obliczenia dają 95% pewności, że prawdziwa wartość β mieści się między βlower i βupper. 
+                Ponadto nasze obliczenia dają 95% pewności, że prawdziwa wartość η mieści się między ηlower i ηupper. 
+                Granice ufności są zazwyczaj obliczane po dopasowaniu krzywej.\n
+                Najlepszym sposobem na zwiększenie granic ufności jest zebranie większej ilości danych. W miarę gromadzenia większej ilości danych, rozkłady β i η są lepiej zdefiniowane
+
+                """)
 if opcje == 'Ręczne wprowadzenie danych':
     st.subheader('Analiza Weibulla dla danych wprowadzanych ręcznie')
     st.markdown(''' --- ''')
